f3e71aab4c4893c9a37eeaf34bef4d6fd2fc1ae1
michael <khavkin@post.bgu.ac.il>
2019-02-25 16:34:48 +0200

    Bug fix in pre-processing and cleaning

--- DataPreProcessor.py
+++ DataPreProcessor.py
@@ -10,134 +10,141 @@ def load_data(data_file_path):
 
 
 def clean_data(data_df, data_types):
+    """
+    Clean input data.
+    Cleaning consists of transforming features (columns) to its right format, filling missing values and verifying the type of each column.
+    :param data_df: Data input
+    :param data_types: Data types for each column in data
+    :return: DataFrame for clean data
+    """
     cleaned_data_df = data_df.copy()
     # Separate last name from first name and affiliation
-    cleaned_data_df['Last_Name'] = cleaned_data_df['Name'].apply(lambda name: get_last_name(name), axis=1)
-    cleaned_data_df['First_Name'] = cleaned_data_df['Name'].apply(lambda name: get_first_name(name), axis=1)
-    cleaned_data_df['Name_Affiliation'] = cleaned_data_df['Name'].apply(lambda name: get_affiliation(name), axis=1)
-    cleaned_data_df = cleaned_data_df.drop(columns=['Name'])
+    data_names = cleaned_data_df['Name'].apply(lambda name: get_affiliation_first_last_name(name)).apply(pd.Series)
+    data_names.columns = ['Name_Affiliation', 'Last_Name', 'First_Name']
+    cleaned_data_df = pd.concat([cleaned_data_df[:], data_names.apply(pd.Series)[:]], axis=1)
+    cleaned_data_df = cleaned_data_df.drop(labels=['Name'], axis=1)
     # Correct age
-    cleaned_data_df['Age'] = cleaned_data_df['Age'].apply(lambda age: round_age(age), axis=1)
+    cleaned_data_df['Age'] = cleaned_data_df['Age'].apply(lambda age: round_age(age))
     # Separate ticket number from ticket code
-    cleaned_data_df['Ticket_Code'] = cleaned_data_df['Ticket'].apply(lambda ticket: get_ticket_code(ticket), axis=1)
-    cleaned_data_df['Ticket_Number'] = cleaned_data_df['Ticket'].apply(lambda ticket: get_ticket_number(ticket), axis=1)
-    cleaned_data_df = cleaned_data_df.drop(columns=['Ticket'])
+    data_tickets = cleaned_data_df['Ticket'].apply(lambda ticket: get_ticket_code_and_number(ticket)).apply(pd.Series)
+    data_tickets.columns = ['Ticket_Code', 'Ticket_Number']
+    cleaned_data_df = pd.concat([cleaned_data_df[:], data_tickets.apply(pd.Series)[:]], axis=1)
+    cleaned_data_df = cleaned_data_df.drop(labels=['Ticket'], axis=1)
     # Separate cabin floor from number
-    cleaned_data_df['Cabin_Floor'] = cleaned_data_df['Cabin'].apply(lambda cabin: get_cabin_floor(cabin), axis=1)
-    cleaned_data_df['Cabin_Rooms'] = cleaned_data_df['Cabin'].apply(lambda cabin: get_cabin_number(cabin), axis=1)
-    cleaned_data_df = cleaned_data_df.drop(columns=['Cabin'])
+    data_cabins = cleaned_data_df['Cabin'].apply(lambda cabin: get_cabin_floor_and_number(cabin)).apply(pd.Series)
+    data_cabins.columns = ['Cabin_Floor', 'Cabin_Rooms']
+    cleaned_data_df = pd.concat([cleaned_data_df[:], data_cabins.apply(pd.Series)[:]], axis=1)
+    cleaned_data_df = cleaned_data_df.drop(labels=['Cabin'], axis=1)
 
-    cleaned_data_df = verify_data_types(cleaned_data_df, data_types)
     cleaned_data_df = check_for_missing_values(cleaned_data_df)
+    cleaned_data_df = verify_data_types(cleaned_data_df, data_types)
+    assert all(col for col in ~cleaned_data_df.isnull())
     return cleaned_data_df
 
 
 def verify_data_types(data_df, data_types):
+    """
+    Convert all column in dataframe to its correct types (Object, int32, or category)
+    :param data_df: Data input
+    :param data_types: Data types for each column in data
+    :return: DataFrame for clean data
+    """
     converters = {
-        'Numerical': lambda col: data_df[col].astype(int),
-        'Text': lambda col: data_df[col].astype('str'),
-        'Categorical': lambda col: data_df[col].astype('category')
+        'Numerical': lambda df, col: df[col].astype(int),
+        'Text': lambda df, col: df[col].astype('str'),
+        'Categorical': lambda df, col: df[col].astype('category')
     }
 
     for column in data_df.columns:
         if column in data_types:
-            converters[data_types[column]](column)
+            data_df[column] = converters[data_types[column]](data_df, column)
     return data_df
 
 
 def check_for_missing_values(data_df):
+    """
+    Fill missing values according to the type of each column
+    :param data_df: Data input
+    :return: DataFrame for clean data
+    """
     for column in data_df:
-        if column.dtype in ['int64', 'float64']:
-            data_df = data_df[column].loc[data_df[column].isnull()].fillna(np.mean(data_df[column]))
-        else:
-            data_df = data_df[column].loc[data_df[column].isnull()].fillna('Unknown')
+        missing_data = data_df[column].loc[data_df[column].isnull()]
+        if len(missing_data) > 0:
+            if data_df[column].dtype in ['int64', 'float64']:
+                data_df[column] = data_df[column].fillna(np.mean(data_df[column]))
+            else:
+                data_df[column] = data_df[column].fillna('Unknown')
     return data_df
 
 
-def get_last_name(x):
-    if not x or ',' not in x:
-        return 'Unknown'
+def get_affiliation_first_last_name(x):
+    """
+    Separate written name from first, last and personal affiliation (e.g., Braund, Mr. Owen Herris ->  Mr. Braund, Owen Herris)
+    :param x: Name
+    :return: [<Personal affiliation>, <Last Name>, <First Name>]
+    """
+    if (not x) or (',' not in x):
+        return ['Unknown', 'Unknown', 'Unknown']
     else:
         name = x.split(',')
-        return name[0].strip()
-
-
-def get_first_name(x):
-    if not x or ',' not in x:
-        return 'Unknown'
-    if '.' not in x:
-        return 'Unknown'
-    else:
-        name = x.split(',')
+        last_name = name[0].strip()
         first_name_with_affiliation = name[1]
-        first_name = first_name_with_affiliation.split('.')
-        return first_name[1].strip()
-
-
-def get_affiliation(x):
-    if not x or ',' not in x:
-        return 'Unknown'
-    if '.' not in x:
-        return 'Unknown'
-    else:
-        name = x.split(',')
-        first_name_with_affiliation = name[1]
-        first_name = first_name_with_affiliation.split('.')
-        return first_name[0].strip().replace('.', '')
+        first_name_with_affiliation_list = first_name_with_affiliation.split('.')
+        first_name = first_name_with_affiliation_list[1].split('(')[0].strip()
+        if not first_name:
+            first_name = 'Unknown'
+        if '.' not in x:
+            affiliation = 'Unknown'
+        else:
+            affiliation = first_name_with_affiliation_list[0].strip().replace('.', '')
+        return [affiliation, last_name, first_name]
 
 
 def round_age(age):
-    return int(np.ceil(age))
+    try:
+        return int(np.ceil(age))
+    except ValueError:
+        return
 
 
-def get_ticket_number(x):
+def get_ticket_code_and_number(x):
+    """
+    Separate ticket code from ticket number (e.g., PC 144254 -> code PC number 144254)
+    :param x: Ticket
+    :return: [<Ticket code>, <Ticket number>]
+    """
     if not x:
-        return 0  # Standard ticket
+        return 'ST', 0
+    if ' ' not in x and all(char.isdigit() for char in x):
+        return 'ST', x
     if ' ' not in x and any(char.isalpha() for char in x):
-        return 0
-    if ' ' not in x and all(char.isdigit() for char in x):
-        return x
-    else:
-        ticket = x.split(' ')
-        return int(ticket[1].strip())
+        return x, 0
+    ticket = x.split(' ')
+    if len(ticket) == 3:
+        ticket[0] = ticket[0] + ticket[1]
+        ticket[1] = ticket[-1]
+    ticket_code = ''.join(c for c in ticket[0] if c.isalnum()).upper()
+    ticket_number = int(ticket[1].strip())
+    return [ticket_code, ticket_number]
 
 
-def get_ticket_code(x):
-    if not x:
-        return 'ST'  # Standard ticket
-    if ' ' not in x and all(char.isdigit() for char in x):
-        return 'ST'
-    else:
-        ticket = x.split(' ')
-        ticket_code = ''.join(c for c in ticket if c.isalnum())
-        return ticket_code
-
-
-def get_cabin_floor(x):
+def get_cabin_floor_and_number(x):
+    """
+    Separate cabin floor from cabin number (e.g., C45 -> floor C room 45)
+    :param x: Cabin
+    :return: [<Cabin floor>, <Cabin number>]
+    """
     all_assigned_cabins = []
-    if not x:
-        return 'Unknown'
-    else:
-        all_cabin_nums = x.split(' ')
-        for cabin in all_cabin_nums:
-            split_cabin_num = re.split('[a-zA-Z]', cabin)
-            if len(split_cabin_num) == 1:
-                continue
-            else:
-                all_assigned_cabins.append(split_cabin_num[0].strip())
-        return ''.join(set(all_assigned_cabins))
-
-
-def get_cabin_number(x):
     all_assigned_cabin_numbers = []
-    if not x:
-        return 'Unknown'
+    if pd.isnull(x):
+        return 'Unknown', 'Unknown'
     else:
         all_cabin_nums = x.split(' ')
         for cabin in all_cabin_nums:
-            split_cabin_num = re.split('[a-zA-Z]', cabin)
+            split_cabin_num = re.findall(r'[^\W\d_]+|\d+', cabin)
+            all_assigned_cabins.append(split_cabin_num[0].strip())
             if len(split_cabin_num) == 1:
-                continue
+                all_assigned_cabin_numbers.append('0')
             else:
                 all_assigned_cabin_numbers.append(split_cabin_num[1].strip())
-        return '-'.join(all_assigned_cabin_numbers)
+        return [''.join(set(all_assigned_cabins)), '-'.join(all_assigned_cabin_numbers)]
--- Main.py
+++ Main.py
@@ -2,7 +2,8 @@ import DataPreProcessor
 
 
 def main():
-    data_file_path = 'train.csv'
+    data_train_file_path = 'train.csv'
+    data_test_file_path = 'test.csv'
     data_types = {'Survived': 'Categorical',
                   'Pclass': 'Categorical',
                   'Name_Affiliation': 'Categorical',
@@ -18,7 +19,20 @@ def main():
                   'Cabin_Floor': 'Categorical',
                   'Cabin_Rooms': 'Text',
                   'Embarked': 'Categorical'}
-    DataPreProcessor.load_data(data_file_path, data_types)
+    print('Loading train data...')
+    train_data_df = DataPreProcessor.load_data(data_train_file_path)
+    print('Loading test data...')
+    test_data_df = DataPreProcessor.load_data(data_test_file_path)
+
+    print('Cleaning and transforming train data...')
+    cleaned_train_data_df = DataPreProcessor.clean_data(train_data_df, data_types)
+    print('Cleaning and transforming test data...')
+    cleaned_test_data_df = DataPreProcessor.clean_data(test_data_df, data_types)
+
+    print('Saving cleaned train data to file...')
+    cleaned_train_data_df.to_csv("clean_train.csv", index=False)
+    print('Saving cleaned test data to file...')
+    cleaned_test_data_df.to_csv("clean_test.csv", index=False)
 
 
 if __name__ == '__main__':
